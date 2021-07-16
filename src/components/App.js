573501e432220035999bf1252bc50f7085a3cfd2
JASPREET SINGH <38997592+Jaspreet8843@users.noreply.github.com>
2021-07-16 14:28:43 +0530

    1 to 1 connection

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,21 @@ function App() {
   const myID = useRef('');                                  //stores current user ID
   const toUserRef = useRef();                               //stores ID of the other user
   const messageRef = useRef();                              //stores data from message input field
+  const sendRef = useRef();
+  const pasteRef = useRef();
   const peer = useRef(new window.peerjs.Peer());            //peer object. ID changes each time it runs.
   const conn = useRef();                                    //connection object gets generated once connection is established
+  const connectConn = useRef();
+  const disconnectConn = useRef();
   const receivedMessage = useRef('');                       //stores received message data
-  const connections = useRef([]);                           //if more than 1 incomming connection then it gets stored in array(only useful for sending)
   const myVideo = useRef();                                 //mediaStream (video) of current user
   const receivedVideo = useRef();                           //mediaStrean (video) of other user
   const calls = useRef();                                   //calls object generated once (video)call made or received
   const notification = useRef();                            //handles notifications
-
+  const answerCall = useRef();
+  const rejectCall = useRef();
+  const endCall = useRef();
+  const makeCall = useRef();
 //######################## USE EFFECT ###########################
 
   useEffect(()=>{
@@ -33,15 +39,19 @@ function App() {
       
       myID.current.innerHTML = id;                          //displays current user's peer ID on the page
       
-      peer.current.on('call', function(call) {                //executes in the event a call is in progress (received)
+      peer.current.on('call', function(call) {                //executes in the event a call is received but not accepted
         calls.current=call;
-        calls.current.answer(myVideo.current.srcObject);      //answers the call and passes current user's video stream
-        onReceiveStream();                                    //sets receivedVideo video output element to the received stream
+        answerCall.current.style.display = "inline";
+        rejectCall.current.style.display = "inline";
+        makeCall.current.style.display = "none";
+        notification.current.innerHTML = "INCOMMING CALL... PLEASE ANSWER OR REJECT.";
+        setTimeout(() => { notification.current.innerHTML = ""}, 4000);
+        //calls.current.answer(myVideo.current.srcObject);      //answers the call and passes current user's video stream
+        //onReceiveStream();                                    //sets receivedVideo video output element to the received stream
       });
 
       peer.current.on('connection', function(connection){     //event when connection is received
         conn.current=connection;                              //start connection
-        connections.current.push(connection);
         SendReceive();
         toUserRef.current.value=conn.current.peer;
       });
@@ -55,58 +65,68 @@ function App() {
 
 
   function SendReceive(){
-    connections.current.forEach((element) => {                                 //run listener for all connections
-      element.on('open',()=>{
-        connections.current.forEach((ele) => {
-          if(element.peer!==ele.peer){
-            element.send({connections:ele.peer});                              //send my connections to all connections
-          }
-        });
+    conn.current.on('open',()=>{
 
-        element.on('data', function(data){
-          if(data.control === 'disconnectCall'){                                 
-            calls.current.close();                                                         //workaround to disconnect call bug
-          }
-          if(data.message)
-          {
-            receivedMessage.current.innerText = "Received: "+data.message;                //displays received data
-          }
-          if(data.connections)
-          {
-            // var flag = 0;
-            // connections.current.forEach((e)=>{if(data.connections===e.peer){flag=1;}});
-            // if(flag===0){
-            if(connections.current.findIndex(x => x.peer===data.connections)===-1){      //if received connection is not found in connections array
-              conn.current = peer.current.connect(data.connections);                    //then connect the connection
-              connections.current.push(conn.current);                                   //and add the connection to the connections array
-              SendReceive();
-            }                      
-          }
-        })
-        notification.current.innerHTML = "CONNECTION ESTABLISHED";             //notifies user that connection is established
-        setTimeout(() => { notification.current.innerHTML = ""}, 4000);        //hides the notification after 4 secs
+      toUserRef.current.style.display = "none";
+      connectConn.current.style.display = "none"
+      disconnectConn.current.style.display = "inline";
+      pasteRef.current.style.display = "none";
+      sendRef.current.style.display = "inline";
+      messageRef.current.style.display = "inline";
+      makeCall.current.style.display = "inline";
+
+      conn.current.on('data', function(data){
+        if(data.control === 'disconnectCall'){                                 
+          calls.current.close();                                                         //workaround to disconnect call bug
+        }
+        if(data.message)
+        {
+          receivedMessage.current.innerHTML += "<p>Received: "+data.message+"</p>";                //displays received data
+        }
       })
-      element.on('close', () => {
-          receivedVideo.current.style.display="none";                            //hides video element
-          notification.current.innerHTML = "CONNECTION CLOSED";                  //notifies user that connection is closed
-          if(connections.current.indexOf(element)>-1){
-            connections.current.splice(connections.current.indexOf(element),1);  //removes the closed connection from connections array
-          }
-          setTimeout(() => { notification.current.innerHTML = ""}, 4000);        //hides the notification after 4 secs
-        })
-    });
+      notification.current.innerHTML = "CONNECTION ESTABLISHED";             //notifies user that connection is established
+      setTimeout(() => { notification.current.innerHTML = ""}, 4000);        //hides the notification after 4 secs
+    })
+    conn.current.on('close', () => {
+      
+      receivedVideo.current.style.display="none";                            //hides video element
+      toUserRef.current.style.display = "inline";
+      connectConn.current.style.display = "inline"
+      disconnectConn.current.style.display = "none";
+      pasteRef.current.style.display = "inline";
+      sendRef.current.style.display = "none";
+      messageRef.current.style.display = "none";
+      makeCall.current.style.display = "none";
+      endCall.current.style.display = "none";
+      answerCall.current.style.display = "none";
+      rejectCall.current.style.display = "none";
+      myVideo.current.style.display="none";
+      receivedMessage.current.innerHTML = "";
+
+      notification.current.innerHTML = "CONNECTION CLOSED";                  //notifies user that connection is closed
+      setTimeout(() => { notification.current.innerHTML = ""}, 4000);        //hides the notification after 4 secs
+      conn.current = '';
+    })
   }
 
   //-------------------------------------------
 
   function onReceiveStream(){
-    receivedVideo.current.style.display="inline";                  //unHide the received video element
     calls.current.on('stream', function(stream){
+      myVideo.current.style.display = "inline";
+      receivedVideo.current.style.display = "inline";
+      makeCall.current.style.display = "none";
+      answerCall.current.style.display = "none";
+      rejectCall.current.style.display = "none";
+      endCall.current.style.display = "inline";
       receivedVideo.current.srcObject = stream;                    //displays received video
     });
 
     calls.current.on('close', () => {
       receivedVideo.current.style.display="none";                   //hides video element
+      myVideo.current.style.display = "none";
+      makeCall.current.style.display = "inline";
+      endCall.current.style.display = "none";
       notification.current.innerHTML = "CALL ENDED";                          //notifies user that call has ended
       setTimeout(() => { notification.current.innerHTML = ""}, 4000);        //hides the notification after 4 secs
     })
@@ -116,7 +136,6 @@ function App() {
 
   const handleConnect = (e) => {
     conn.current=peer.current.connect(toUserRef.current.value);    //connects to the other user
-    connections.current.push(conn.current);                        //appends the connection to the array connections
     SendReceive();
   }
 
@@ -125,24 +144,36 @@ function App() {
   const handleDisconnect = (e) => {
     if (calls.current){calls.current.close()}
     conn.current.close();
-    connections.current.forEach((element) => {element.close()})     //disconnects from all connections
-    connections.current = [];                                       //clears the connections array
-
   }
   //-------------------------------------------
 
   const handleSend = (e) => {
-    connections.current.forEach((element)=>{element.send({message:messageRef.current.value})})  //sends the data to each connection
+    if (conn.current){
+      conn.current.send({message:messageRef.current.value})
+    }
+    else{
+      notification.current.innerHTML = "CONNECT TO A PEER TO SEND DATA";     //notifies user that no connections are available
+      setTimeout(() => { notification.current.innerHTML = ""}, 4000);        //hides notification after 4 secs
+    }
     messageRef.current.value = "";
     console.clear();
-    connections.current.forEach((e)=>{console.log(e.peer);})
+    console.log(conn.current.peer);
   }
 
   //-------------------------------------------
 
   const handleCall = (e) => {
-    calls.current = peer.current.call(toUserRef.current.value, myVideo.current.srcObject);  //calls the other user
-    onReceiveStream();                                                                      //displays the stream of the other user
+    if(conn.current)
+    {
+      calls.current = peer.current.call(conn.current.peer, myVideo.current.srcObject);  //calls the other user
+      notification.current.innerHTML = "CALLING...";
+      setTimeout(() => { notification.current.innerHTML = ""}, 4000);
+      onReceiveStream();                                                                //displays the stream of the other user
+    }
+    else{
+      notification.current.innerHTML = "CONNECT TO A PEER TO CALL";              //notifies user that no connections are available
+      setTimeout(() => { notification.current.innerHTML = ""}, 4000);        //hides notification after 4 secs
+    }
   }
 
   //-------------------------------------------
@@ -150,6 +181,23 @@ function App() {
   const handleDisconnectCall = (e) => {
     conn.current.send({control:"disconnectCall"})                       //sends a user defined control "disconnectCall" along the message
     calls.current.close();                                              //disconnects the call
+  }
+  
+  //-------------------------------------------
+
+  const handleAnswerCall = (e) => {
+    calls.current.answer(myVideo.current.srcObject);
+    onReceiveStream();
+  }
+
+  //-------------------------------------------
+
+  const handleRejectCall = (e) => {
+    answerCall.current.style.display = "none";
+    rejectCall.current.style.display = "none";
+    makeCall.current.style.display = "inline";
+    calls.current.close();
+    onReceiveStream();
   }
 
 //####################### RENDER ############################
@@ -159,14 +207,16 @@ function App() {
       <p ref={myID}></p>
       <p><button onClick={() => {navigator.clipboard.writeText(myID.current.innerHTML)}}>COPY ID</button></p>
       <p ref={receivedMessage}></p>
-      <button onClick={() => {navigator.clipboard.readText().then(clipText => toUserRef.current.value =clipText)}}>PASTE</button>     
-      <input type="text" placeholder="to" ref={toUserRef}/>
-      <button onClick={handleConnect}>CONNECT</button>
-      <button onClick={handleDisconnect}>DISCONNECT</button>
-      <input type="text" placeholder="message" ref={messageRef}/>
-      <button onClick={handleSend}>SEND</button>
-      <button onClick={handleCall}>CALL</button>
-      <button onClick={handleDisconnectCall}>END CALL</button>
+      <button ref={pasteRef} onClick={() => {navigator.clipboard.readText().then(clipText => toUserRef.current.value =clipText)}}>PASTE</button>     
+      <input ref={toUserRef} type="text" placeholder="to"/>
+      <button ref={connectConn} onClick={handleConnect}>CONNECT</button>
+      <button ref={disconnectConn} onClick={handleDisconnect} hidden>DISCONNECT</button>
+      <input ref={messageRef} type="text" placeholder="message" hidden/>
+      <button ref={sendRef} onClick={handleSend} hidden>SEND</button>
+      <button ref={makeCall} onClick={handleCall} hidden>CALL</button>
+      <button ref={answerCall} onClick={handleAnswerCall} hidden>ANSWER</button>
+      <button ref={rejectCall} onClick={handleRejectCall} hidden>REJECT</button>
+      <button ref={endCall} onClick={handleDisconnectCall} hidden>END CALL</button>
       <br/>
       <p  ref={notification}></p>
       <br/>
